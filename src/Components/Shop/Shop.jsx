c2b628440c65c779e87ccddb786474a8cee71499
Naiem Islam <naiemislam1547@gmail.com>
2023-07-08 12:56:07 +0600

    added handler button

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,6 +4,8 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect( () =>{
         fetch('products.json')
         .then(res => res.json())
@@ -11,8 +13,9 @@ const Shop = () => {
     } ,[]);
 
     const handleAddToCart = (product) => {
-        console.log(product);
-    }
+       const newCart = [...cart, product];
+       setCart(newCart);
+    };
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -26,6 +29,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h3>This is Order Summary</h3>
+                <p>Selected Item: {cart.length}</p>
             </div>
         </div>
     );
